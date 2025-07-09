f4184b969452d05999421ccc8468ae79bf87f1c5
YanniCastle <yannicastle5@gmail.com>
2025-07-09 04:04:16 -0600

    inicio

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>yannicastle</title>
+  <link rel="stylesheet" href="css/index.css" />
+</head>
+
+<body>
+  <h1>Bienvenido al sitio de Yanni Castle</h1>
+</body>
+
+</html>
